821db667cb18b3ccf0f47237ead955c1b39b92b1
Ubuntu <Chandu@BDEDeepLearning.asrd3uaare3efa1rjhr3yw1sde.bx.internal.cloudapp.net>
2018-02-18 22:28:25 +0000

    changed output dir

--- tools/demo.py
+++ tools/demo.py
@@ -70,7 +70,7 @@ def vis_detections(image_name,im, class_name, dets, thresh=0.5):
     plt.axis('off')
     plt.tight_layout()
     #plt.draw()
-    plt.savefig(image_name)
+    plt.savefig("output_images/"+image_name)
 
 def demo(net, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
